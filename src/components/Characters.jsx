b26f6d01884a99a12b4694dbb303ea347002731f
jose_pirela <jjpirela93@gmail.com>
2021-08-04 17:42:36 -0500

    clase 9, useRef

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useMemo } from "react";
+import React, { useState, useEffect, useReducer, useMemo, useRef } from "react";
 
 const initialState = {
   favorites: [],
@@ -21,6 +21,7 @@ const Characters = () => {
   const [characters, setCharacters] = useState([]);
   const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
   const [search, setSearch] = useState("");
+  const searchInput = useRef(null);
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character/")
@@ -34,9 +35,11 @@ const Characters = () => {
   const handleClick = (favorite) => {
     dispatch({ type: "ADD_TO_FAVORITE", payload: favorite });
   };
-  const handleSearch = (event) => {
-    setSearch(event.target.value);
+
+  const handleSearch = () => {
+    setSearch(searchInput.current.value);
   };
+
   const filteredCharacters = useMemo(
     () =>
       characters.filter((character) => {
@@ -57,7 +60,7 @@ const Characters = () => {
       ))}
 
       <div className="search">
-        <input type="text" value={search} onChange={handleSearch} />
+        <input type="text" value={search} ref={searchInput} onChange={handleSearch} />
       </div>
 
       {filteredCharacters.map((character) => (
